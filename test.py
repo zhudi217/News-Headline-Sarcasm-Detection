9aae3dedaf4b5d7caaa04f65d72507ddaf176a58
ThisIsNotAValidUserName <zhudi217@foxmail.com>
2019-05-11 22:27:40 -0400

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-
-sent = "Rescuers heoically help beached garbage back into ocean"
-analyzer = SentimentIntensityAnalyzer()
-
-senti = analyzer.polarity_scores(sent)
-
-print(senti)
